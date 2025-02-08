99c5aca143a39dd07ad535c6b96cd4f2e5595fff
Venktesh123 <8303531530venktesh@gmail.com>
2025-02-08 15:12:41 +0530

    C

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://b-backend-chi.vercel.app//api";
+const API_URL = "https://b-backend-chi.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
